00ff5ec53219bf92cceb9c4d853885c4abc8d499
phamngoctuong1805 <phamngoctuong1805@gmail.com>
2020-01-08 00:11:37 +0700

    Create class axiosService

---
+++ src/commons/axiosService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+class axiosService {
+	constructor() {
+	  const instance = axios.create();
+	  this.instance = instance;
+	}
+	handleSuccess(response) {
+		return response;
+	}
+	handleError(error) {
+		return Promise.reject(error);
+	}
+	get(url) {
+		return this.instance.get(url)
+	}
+}
+export default new axiosService();
