a4ea4fc36776727a35760f79ca35155d3ac34443
phamngoctuong1805 <phamngoctuong1805@gmail.com>
2020-01-07 23:17:39 +0700

    TaskBoard Full

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,15 +1,8 @@
 import React, { Component, } from 'react';
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-import {STATUSES} from './../../constants';
 import Typography from '@material-ui/core/Typography';
-import TaskList from './../../components/TaskList';
-import TaskForm from './../../components/TaskForm';
+import TaskBoard from './../TaskBoard';
 import { Provider } from 'react-redux';
 import configureStore from './../../redux/configureStore';
 const store = configureStore();
@@ -30,38 +23,11 @@ class App extends Component {
       open: true
     }) 
   }
-  renderBoard() {
-    let xhtml = null;
-    xhtml = (
-      STATUSES.map((status,index)=> {
-        return(
-          <TaskList key={index} status={status} index={index}/>
-        );
-      })
-    );
-    return xhtml
-  }
-  renderForm() {
-    var {open} = this.state;
-    let xhtml = null;
-    xhtml = <TaskForm handleClose={this.handleClose} open={open} />
-    return xhtml;
-  }
   render() {
-    var {classes} = this.props;
+    var {open} = this.state;
     return (
       <Provider store={store}>
-        <Container maxWidth="lg">
-          <div className="App">
-            <Button variant="contained" color="primary" onClick={this.openTask}><AddIcon/>&nbsp;Thêm công việc mới</Button>
-            <Box component="div" mt={2}>
-              <Grid container className={classes.root} spacing={1}>
-                {this.renderBoard()}
-                {this.renderForm()}
-              </Grid>
-            </Box>
-          </div>
-        </Container>
+        <TaskBoard open={open} handleClose={this.handleClose} openTask={this.openTask}/>
       </Provider>
     );
   }
